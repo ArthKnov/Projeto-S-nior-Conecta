806fb9c859071730ddba5320eb3334a759a4fa2a
Arthur Lima <arthurlima121213@gmail.com>
2024-06-13 17:11:57 -0300

    menu inserido em todas as paginas

--- Js/scripts.js
+++ Js/scripts.js
@@ -10,21 +10,3 @@ function navigateTo(page) {
     window.location.href = page;
 }
 
-$(document).ready(function() {
-    // Função para exibir o menu ao passar o mouse sobre o ícone do menu
-    $('.header').mouseenter(function() {
-        $('.menu').fadeIn().addClass('animate__fadeIn');
-        $('.menu-icon').fadeOut(200, function() {
-            $(this).attr('src', 'Imagens/Background pra baixo.png').fadeIn(200);
-        });
-    });
-
-    // Função para ocultar o menu ao retirar o mouse do ícone do menu
-    $('.header').mouseleave(function() {
-        $('.menu').fadeOut().removeClass('animate__fadeIn');
-        $('.menu-icon').fadeOut(200, function() {
-            $(this).attr('src', 'Imagens/Background.png').fadeIn(200);
-        });
-    });
-});
-
